3aacc22d8193593006058fa7b18254a38b07683d
Jefferson John <jefferson.john.rego@gmail.com>
2017-09-18 15:35:40 -0300

    Apresenta vazamentos de memoria

--- Grafo/grafo.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GRAFO_H__
-#define __GRAFO_H__
-
-struct grafo{
-	int id;
-	char *nome;
-};
-#endif
--- Grafo/main.c
+++ Grafo/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "grafo.h"
+#include <string.h>
+
+typedef struct grafo_{
+	int id;
+	char *nome;
+} grafo;
 
 int main(){
 	FILE* file = fopen("mapa.txt", "r");
@@ -8,9 +13,29 @@ int main(){
 		printf("Error openning file!\n");
 		exit(1);
 	}
-	int ncid;
-	fscanf(file,"%d\n", &ncid);
-	
+	int nv;
+	char name[256];
+	fscanf(file,"%d\n", &nv);
+	grafo cidades[nv];
+	for(int i=0;i<nv;i++){
+		cidades[i].id = i+1;
+		fgets(name, sizeof(name), file);
+		name[strlen(name)-1] = '\0';
+		cidades[i].nome = strdup(name);;
+	}
+	int m[nv][nv];
+	for (int i=0;i<nv;i++){
+		for (int j=0;j<nv;j++){
+			m[i][j] = 0;
+		}
+	}
+	int na;
+	fscanf(file,"%d\n", &na);
+	for (int i=0;i<na;i++){
+		int a,b;
+		fscanf(file,"%d %d\n", &a, &b);
+		m[a-1][b-1] = 1;
+	}
 	fclose(file);
 	char opt;
 	while (1<2){
@@ -18,15 +43,37 @@ int main(){
 		scanf(" %c", &opt);
 		if(opt == 'q')
 			break;
+		int num;
 		switch(opt) {
 			case 'a':
-				/*o programa deve ler um indice numerico de uma cidade e listar os nomes das cidades adjacentes*/
+				scanf("%d", &num);
+				if (num>nv){
+					printf("id nao existe\n");
+				}
+				else{
+					for (int i=0;i<nv;i++){
+						if (m[num-1][i]>0){
+							printf("%s, ", cidades[i].nome);
+						}
+					}
+					printf("\n");
+				}
 			break;
 			case 'd':
-				/*o programa deve gerar o grafo .dot do graphviz na saida padrao*/
+				printf("digraph {\n");
+				for (int i=0;i<nv;i++){
+					for (int j=0;j<nv;j++){
+						if (m[i][j]>0){
+							printf("\t%s -> %s;\n", cidades[i].nome, cidades[j].nome);
+						}
+					}
+				}
+				printf("}\n");
 			break;
 			case 'l':
-				/*o programa deve mostrar a lista decidades com seus id's*/
+				for (int i=0;i<nv;i++){
+					printf("%d: %s\n", cidades[i].id, cidades[i].nome);
+				}
 			break;
 			default:
 			break;
